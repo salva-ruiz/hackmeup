f4026b6e12bba3080f5e7d6969202b4b7413d622
Salvador Ruiz <info@ok-computer.es>
2019-03-23 12:58:16 +0100

    7º Mostrar filtrado por el titulo=apple los productos que aparecen en el fichero /products.json, mostrando título, imagen y precio.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.order('code')
+    @search = params[:title]
+    @products = @products.where(["lower(name) = ?", @search.downcase]) unless @search.blank?
   end
 
 end
