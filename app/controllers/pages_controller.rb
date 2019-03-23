9378148e804186bfcc8b382352999e8103743d0b
Salvador Ruiz <info@ok-computer.es>
2019-03-23 11:33:19 +0100

    2º Desarrollar una página de bienvenida, devolver por defecto “Hello world!” y si el navegador está configurado en idioma español “¡Hola mundo!” en la ruta “/welcome”

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def health_check
     @timestamp = Time.current
   end
+
+  def welcome
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#welcome'
+  get '/welcome', to: 'pages#welcome'
   get '/health-check', to: 'pages#health_check'
 end
