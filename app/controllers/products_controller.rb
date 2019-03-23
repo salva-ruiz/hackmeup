d5def15484dd3851705d6a141e43e220a3514481
Salvador Ruiz <info@ok-computer.es>
2019-03-23 13:22:08 +0100

    8º  Listar los productos que aparecen en el fichero /products.json, mostrando título, imagen y precio, ordenados dependiento de los parámetros _sort y _order.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.order('code')
+    @order = 'asc'
+    @order = 'desc' if params[:_order] == 'desc'
+
+    @products = Product.order("name #{@order}")
+    @sort = 'title'
+    if params[:_sort] == 'price'
+      @products = @products.reorder("price #{@order}")
+      @sort = 'price'
+    end
+
     @search = params[:title]
     @products = @products.where(["lower(name) = ?", @search.downcase]) unless @search.blank?
+    @products = @products.order('name')
+    
   end
 
 end
