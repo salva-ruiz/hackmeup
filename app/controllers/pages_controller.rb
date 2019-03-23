7edee1868eafe00ff27f4f8d34c079d4fc14bd3f
Salvador Ruiz <info@ok-computer.es>
2019-03-23 11:45:55 +0100

    3º Desarrollar una página simple de login con los campos “email” y “password” y el botón “Enviar”  en la  ruta “/login”

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,7 @@ class PagesController < ApplicationController
 
   def welcome
   end
+
+  def login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#welcome'
   get '/welcome', to: 'pages#welcome'
   get '/health-check', to: 'pages#health_check'
+  get '/login', to: 'pages#login'
 end
